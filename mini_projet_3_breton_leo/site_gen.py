a98be28c92646ec055f3bbf9857876623a3acfb9
LeoZenin <breton.leo49@gmail.com>
2025-02-07 11:14:33 +0100

    Update site_gen.py

--- mini_projet_3_breton_leo/site_gen.py
+++ mini_projet_3_breton_leo/site_gen.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import creation_graph
 
 app = Flask(__name__)
 
 @app.route("/")
 def site_fonction():
-    return "<p>Hello world</p>"
+    return render_template('index.html')
 
 if __name__=="__main__":
     app.run(port=8080)
