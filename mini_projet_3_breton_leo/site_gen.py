75776d0ee0ba587008482397a2dfdf22f22aee9d
LeoZenin <breton.leo49@gmail.com>
2025-03-28 10:52:48 +0100

    Update site_gen.py

--- mini_projet_3_breton_leo/site_gen.py
+++ mini_projet_3_breton_leo/site_gen.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import creation_graph
+import numpy as np
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def site_fonction():
-    return render_template('index.html')
+    args = request.args
+    fonction = args.get('fonction')
+    mon_graphique = 'mon_graphique.png'
+    chemin_graphique = os.path.join("static", "images", mon_graphique) 
+    creation_graph.graphique(chemin_graphique,
+                             tab=[0, 2*np.pi, 60], 
+                             fonction)
+    return render_template('index.html',
+                           chemin_graphique)
 
 if __name__=="__main__":
     app.run(port=8080)
